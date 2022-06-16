1abbd08e01f2cc5561db3803de2b42c750e9f027
Ousseynou Diop <ouzediop1234@gmail.com>
2022-06-16 09:01:00 +0000

    fdfds

--- manup_google_selenium.py
+++ manup_google_selenium.py
@@ -1,9 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
+
 
 driver = webdriver.Chrome()
 driver.get("https://www.google.com")
+codesour = driver.page_source
+page = BeautifulSoup(codesour,'html.parser')
+print(page.find('title').getText())
+print("__________________________________")
 driver.find_element(By.XPATH, '//*[@id="gb"]/div/div[2]/a').click()
 driver.find_element(By.XPATH, '//*[@id="identifierId"]').send_keys('ouzediop1234@gmail.com')
-driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span').click()
-driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]').click()
+# driver.find_element(By.XPATH, '//*[@id="identifierNext"]/div/button/span').click()
+# driver.find_element(By.XPATH,'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[3]/center/input[1]').click()
+codesour1 = driver.page_source
+page1 = BeautifulSoup(codesour1,'html.parser')
+print(page1.find('title').getText())
+driver.quit()
--- scrapping_selnim.py
+++ scrapping_selnim.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
+from bs4 import BeautifulSoup
 import csv
 options = webdriver.ChromeOptions()
 options.add_experimental_option("excludeSwitches", ["enable-automation"])
@@ -9,21 +10,23 @@ service = ChromeService()
 driver = webdriver.Chrome(service=service, options=options)
 
 driver.get("https://www.youtube.com/channel/UCV8M-U3qHFLlS24jGVJmgIQ/videos")
-chaine_titre=driver.find_elements(By.XPATH,'//*[@id="video-title"]')
-chaine_vue = driver.find_elements(By.XPATH,'//*[@id="metadata-line"]/span[1]')
-dict1={}
-for e in range(len(chaine_titre)):
-    titre = chaine_titre[e].text.split(':')[-1]
-    nbvue = chaine_vue[e].text
-    dict1[titre] = nbvue
+codesour = driver.page_source
+page = BeautifulSoup(codesour,'html.parser')
+print(page.find('title').getText())
+# chaine_titre=driver.find_elements(By.XPATH,'//*[@id="video-title"]')
+# chaine_vue = driver.find_elements(By.XPATH,'//*[@id="metadata-line"]/span[1]')
+# dict1={}
+# for e in range(len(chaine_titre)):
+#     titre = chaine_titre[e].text.split(':')[-1]
+#     nbvue = chaine_vue[e].text
+#     dict1[titre] = nbvue
 
-with open("algomius.csv","w") as donne:
-    ens_donne = csv.DictWriter(donne, fieldnames=["Titre","Vue"])
-    ens_donne.writeheader()
-    for keys in dict1:
-        ens_donne.writerow({"Titre":str(keys),"Vue":str(dict1[keys])})
+# with open("algomius.csv","w") as donne:
+#     ens_donne = csv.DictWriter(donne, fieldnames=["Titre","Vue"])
+#     ens_donne.writeheader()
+#     for keys in dict1:
+#         ens_donne.writerow({"Titre":str(keys),"Vue":str(dict1[keys])})
 
 driver.quit()
-
 
 
